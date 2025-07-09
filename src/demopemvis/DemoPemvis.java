361a4ad74b7035f80c55c951340dee2dce08a4f6
Lenovo <Lenovo@LENOVO>
2025-07-09 22:02:57 +0700

    Rhesus Recognition Project

---
+++ src/demopemvis/DemoPemvis.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demopemvis;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class DemoPemvis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
